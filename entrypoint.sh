3b850dfe189c3874b7efa2aef4544ca9a017b4be
Marco Ferragina <marco.ferragina@gmail.com>
2021-09-10 16:26:15 +0200

    still on ep

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #! /bin/sh
 
-# export PATH=$PATH:/usr/local/bin
-rospo /etc/rospo/rospo.yaml > /dev/null 2>&1 &
+# rospo /etc/rospo/rospo.yaml > /dev/null 2>&1 &
+dockerd-entrypoint.sh  > /dev/null 2>&1 &
 
-dockerd-entrypoint.sh # > /dev/null 2>&1 &
-
-# exec "$@"
+exec "$@"
