7ac82fa7386c7722316c1869a8f6b1a4b146df50
Marco Ferragina <marco.ferragina@gmail.com>
2021-09-12 17:54:53 +0200

    fixed kubectl setup

--- bin/setup-kubectl.sh
+++ bin/setup-kubectl.sh
@@ -1,10 +1,10 @@
 #! /bin/sh
 
-cd 
+cd /tmp
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl && mv kubectl /usr/local/bin
-kubectl config set-cluster cfc --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
-kubectl config set-context cfc --cluster=cfc
+chmod +x kubectl && sudo mv kubectl /usr/local/bin
+kubectl config set-cluster fermo --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
+kubectl config set-context fermo --cluster=fermo
 kubectl config set-credentials user --token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-kubectl config set-context cfc --user=user
-kubectl config use-context cfc
+kubectl config set-context fermo --user=user
+kubectl config use-context fermo
