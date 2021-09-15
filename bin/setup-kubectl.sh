b45da845d43210ae3f697000bc5be2d484f47896
Marco Ferragina <marco.ferragina@gmail.com>
2021-09-15 11:31:24 +0200

    k8s utils

--- bin/setup-kubectl.sh
+++ bin/setup-kubectl.sh
@@ -1,8 +1,5 @@
 #! /bin/sh
 
-cd /tmp
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-chmod +x kubectl && sudo mv kubectl /usr/local/bin
 kubectl config set-cluster fermo --server=https://kubernetes.default --certificate-authority=/var/run/secrets/kubernetes.io/serviceaccount/ca.crt
 kubectl config set-context fermo --cluster=fermo
 kubectl config set-credentials user --token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -15,13 +15,20 @@ setup_packages() {
         byobu \
         psmisc \
         python3-pip \
+        openconnect \
 
     # cleanup
     rm -r /var/lib/apt/lists /var/cache/apt/archives
+    pip install vpn-slice
 }
 
 setup_ibmcloud() {
     cd /tmp
+
+    curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+    chmod +x kubectl && sudo mv kubectl /usr/local/bin
+    curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+    
     curl -fL https://download.clis.cloud.ibm.com/ibm-cloud-cli/${IBMCLOUD_CLI}/binaries/IBM_Cloud_CLI_${IBMCLOUD_CLI}_linux_amd64.tgz -o IBM_Cloud_CLI_${IBMCLOUD_CLI}_linux_amd64.tgz 
     tar -zxf IBM_Cloud_CLI_${IBMCLOUD_CLI}_linux_amd64.tgz
     mv /tmp/IBM_Cloud_CLI/ibmcloud /usr/local/bin
